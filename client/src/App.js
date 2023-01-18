e3148ed2f14157003ca94b0380703a064f856905
ankit2341 <ankitpatil2341@gmail.com>
2023-01-18 14:27:52 +0530

    added all

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import MainRoutes from './Routes/MainRoutes';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MainChatbot from './Components/Chatbot/MainChatbot';
 
 function App() {
   return (
     <div className="App">
       <MainRoutes />
+      <MainChatbot/>
     </div>
   );
 }
