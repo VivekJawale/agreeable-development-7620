4833c61f8f4468bc5c005d81512ae92db0ec6cf3
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-01-21 02:03:39 +0530

    updated css subnav

--- client/src/Pages/Product-Pages/Protiens.jsx
+++ client/src/Pages/Product-Pages/Protiens.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import { Box } from '@chakra-ui/react';
 import ProtienFiltetComp from './ProtienFilterComp';
 import ProtienComp from './ProtienComp';
+import Navbar from '../Navbar';
+import SubNav from '../SubNav';
 
 const Protiens = () => {
   return (
-    <Box display="flex" bg="white" color="black" w="95%" m="auto" justifyContent="space-between" pt="100px">
-      <Box w="19%"> 
-        <ProtienFiltetComp />
-      </Box>
-      <Box w="78%">
-        <ProtienComp />
+    <Box>
+      <Navbar />
+      <SubNav />
+      <Box display="flex" bg="white" color="black" w="95%" m="auto" justifyContent="space-between" pt="100px">
+        <Box w="19%"> 
+          <ProtienFiltetComp />
+        </Box>
+        <Box w="78%">
+          <ProtienComp />
+        </Box>
       </Box>
     </Box>
   )
